9a447f21c9df9b738632e954058236b607dd2548
mostafa0x <dodomido12350@gmail.com>
2026-09-24 23:22:46 +0300

    qw

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   reactCompiler: true,
   images: {
+    unoptimized: true,
     qualities: [75, 85],
-
   },
 };
 
